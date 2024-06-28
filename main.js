f2cf8a400e9fe0edeaf32547ba0e4c0b080851de
Enrique1311 <ejspinelli131176@gmail.com>
2024-06-28 15:52:23 -0300

    History API has been added.

--- main.js
+++ main.js
@@ -1,135 +1,146 @@
-// Date() object
-// ************************
-const newDate = new Date();
-console.log(newDate);
+// // Date() object
+// // ************************
+// const newDate = new Date();
+// console.log(newDate);
 
-console.log(Date.now()); // In milliseconds
+// console.log(Date.now()); // In milliseconds
 
-console.log("Date of the month:" + newDate.getDate());
+// console.log("Date of the month:" + newDate.getDate());
 
-console.log("Day of the week: " + newDate.getDay());
+// console.log("Day of the week: " + newDate.getDay());
 
-console.log("Month: " + newDate.getMonth());
+// console.log("Month: " + newDate.getMonth());
 
-console.log("Year: " + newDate.getYear()); // the year - 1900
+// console.log("Year: " + newDate.getYear()); // the year - 1900
 
-console.log("Full Year: " + newDate.getFullYear());
+// console.log("Full Year: " + newDate.getFullYear());
 
-console.log("Hour: " + newDate.getHours());
+// console.log("Hour: " + newDate.getHours());
 
-console.log("Minutes: " + newDate.getMinutes());
+// console.log("Minutes: " + newDate.getMinutes());
 
-console.log("Seconds: " + newDate.getSeconds());
+// console.log("Seconds: " + newDate.getSeconds());
 
-//**************************************************************************************************************
+// //**************************************************************************************************************
 
-// localStorage()
-//**************************
+// // localStorage()
+// //**************************
 
-// setItem() - Asign the value of the localStorage
-localStorage.setItem("name", "Enrique");
-console.log(localStorage);
+// // setItem() - Asign the value of the localStorage
+// localStorage.setItem("name", "Enrique");
+// console.log(localStorage);
 
-// getItem() - Get the value of the localStorage
-console.log(localStorage.getItem("name"));
+// // getItem() - Get the value of the localStorage
+// console.log(localStorage.getItem("name"));
 
-let myName = localStorage.getItem("name");
-console.log(`Variable which contain name of the localStorage: ${myName}`);
+// let myName = localStorage.getItem("name");
+// console.log(`Variable which contain name of the localStorage: ${myName}`);
 
-// removeItem() - Remove an item of the localStorage
-setTimeout(() => {
-	localStorage.removeItem("name");
-}, 2000);
-
-// clear() - Clear all the values of the localStorage
+// // removeItem() - Remove an item of the localStorage
 // setTimeout(() => {
-// 	localStorage.clear();
+// 	localStorage.removeItem("name");
 // }, 2000);
 
-//**************************************************************************************************************
+// // clear() - Clear all the values of the localStorage
+// // setTimeout(() => {
+// // 	localStorage.clear();
+// // }, 2000);
 
-// sessionStorage()
-//**************************
-sessionStorage.setItem("name", "Javier");
+// //**************************************************************************************************************
 
-let otherName = sessionStorage.getItem("name");
-console.log(otherName);
+// // sessionStorage()
+// //**************************
+// sessionStorage.setItem("name", "Javier");
 
-// ****************************************************************************************************
+// let otherName = sessionStorage.getItem("name");
+// console.log(otherName);
 
-// Drag and Drop (dragstart, drag & dragend events)
-// ********************************************************
+// // ****************************************************************************************************
 
-const $circle = document.querySelector(".circle");
-const $container = document.querySelector(".container");
+// // Drag and Drop (dragstart, drag & dragend events)
+// // ********************************************************
 
-$circle.addEventListener("dragstart", (e) => {
-	console.log("dragstart");
-	console.log(e.dataTransfer);
-	e.dataTransfer.setData("myClass", e.target.className); // It is convenient to work with id
-});
+// const $circle = document.querySelector(".circle");
+// const $container = document.querySelector(".container");
 
-// $circle.addEventListener("drag", () => {
-// 	console.log("drag");
+// $circle.addEventListener("dragstart", (e) => {
+// 	console.log("dragstart");
+// 	console.log(e.dataTransfer);
+// 	e.dataTransfer.setData("myClass", e.target.className); // It is convenient to work with id
 // });
 
-// $circle.addEventListener("dragend", () => {
-// 	console.log("dragend");
+// // $circle.addEventListener("drag", () => {
+// // 	console.log("drag");
+// // });
+
+// // $circle.addEventListener("dragend", () => {
+// // 	console.log("dragend");
+// // });
+
+// // (dragenter, dragover, drop & dragleave)
+// $container.addEventListener("dragenter", () => {
+// 	console.log("dragenter");
 // });
 
-// (dragenter, dragover, drop & dragleave)
-$container.addEventListener("dragenter", () => {
-	console.log("dragenter");
-});
+// $container.addEventListener("dragover", () => {
+// 	e.preventDefault(); // prevent default allows the drop event to be actived on the element
+// 	console.log("dragover");
+// });
 
-$container.addEventListener("dragover", () => {
-	e.preventDefault(); // prevent default allows the drop event to be actived on the element
-	console.log("dragover");
-});
+// $container.addEventListener("drop", (e) => {
+// 	console.log("drop");
+// 	console.log(e.dataTransfer.getData("myClass"));
+// });
 
-$container.addEventListener("drop", (e) => {
-	console.log("drop");
-	console.log(e.dataTransfer.getData("myClass"));
-});
+// $container.addEventListener("dragleave", () => {
+// 	console.log("dragleave");
+// });
 
-$container.addEventListener("dragleave", () => {
-	console.log("dragleave");
-});
+// // **************************************************************************************************
 
-// **************************************************************************************************
+// // Geolocation API
+// // *********************************
 
-// Geolocation API
-// *********************************
+// const geolocat = navigator.geolocation;
+// console.log(geolocat);
 
-const geolocat = navigator.geolocation;
-console.log(geolocat);
+// // getCurrentPosition
+// const position = (pos) => {
+// 	console.log(pos);
+// 	console.log(pos.coords);
+// 	console.log(pos.coords.latitude);
+// 	console.log(pos.coords.longitude);
+// };
 
-// getCurrentPosition
-const position = (pos) => {
-	console.log(pos);
-	console.log(pos.coords);
-	console.log(pos.coords.latitude);
-	console.log(pos.coords.longitude);
-};
+// const err = () => {
+// 	console.log(e);
+// };
 
-const err = () => {
-	console.log(e);
-};
+// const options = {
+// 	maximumAge: 0, // the time of actualisation
+// 	timeOut: 3000, // the time the app will be waiting to show the information
+// 	enableHighAccuracy: true, // active the high presition to find the location
+// };
 
-const options = {
-	maximumAge: 0, // the time of actualisation
-	timeOut: 3000, // the time the app will be waiting to show the information
-	enableHighAccuracy: true, // active the high presition to find the location
-};
+// geolocat.getCurrentPosition(position, err, options);
 
-geolocat.getCurrentPosition(position, err, options);
+// // watchPosition()
+// const watchPosition = navigator.geolocation.watchPosition((pos) => {
+// 	const { latitude, longitude } = pos.coords;
 
-// watchPosition()
-const watchPosition = navigator.geolocation.watchPosition((pos) => {
-	const { latitude, longitude } = pos.coords;
+// 	console.log(`Watching latitude ${latitude} and longitude ${longitude}`);
+// 	console.log(`Wath Position: ${watchPosition}`);
+// });
 
-	console.log(`Watching latitude ${latitude} and longitude ${longitude}`);
-	console.log(`Wath Position: ${watchPosition}`);
-});
+// **********************************************************************************************
 
-watchPosition();
+// history API
+// ********************
+console.log(history);
+console.log(history.length);
+// history.back();
+// history.forward()
+// history.go() or history(0) // reload the the page
+// history.go(1) // to go to one next page
+// history.go(-1) // to go to one previous page
+console.log(location.href);
